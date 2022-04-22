a5c66de1919bf81ee1a2b2bef54b1e722df5a4ff
Paweł Marzec <pm@eoln.eu>
2022-04-22 21:12:55 +0200

    feat: async click with key options from envvar

--- scrub.py
+++ scrub.py
@@ -1,9 +1,18 @@
-import click
+import anyio
+import asyncclick as click
 
+config = {'apikey':''}
 @click.group()
-def cli():
-  pass
+@click.option('--apikey', envvar='GLASSNODE_API_KEY')
+async def cli(apikey):
+  config['apikey'] = apikey
 
 @cli.command()
-def hello():
-  click.echo("Hello World")
+async def endpoints():
+  click.echo(f"apikey:{config['apikey']}")
+  click.echo('endpoints')
+
+
+
+if __name__ == '__main__':
+    cli(_asyncio_backend="asyncio")
